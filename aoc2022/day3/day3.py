d7a31db4afc96ea5b8fe3d3c44845c3b932d9198
Fabio Henriques <fabyohenriques@gmail.com>
2022-12-04 14:23:36 +0000

    AOC2022 Day 3

---
+++ aoc2022/day3/day3.py
@@ -0,0 +1,31 @@
+from string import ascii_letters
+
+#Open and read the input file
+with open('day3_input.txt') as f:
+    lines = f.readlines()
+
+#data separation in lines
+data = [str(line.strip()) for line in lines]
+
+total = 0
+
+for i in data:
+    #Find the middle of rucksack 
+    #'//' returns an integer instead of a double in '/' 
+    half = len(i)//2
+
+    #variable containing the first half
+    #'set' function returns a set of all unique characters 
+    left = set(i[:half])
+    #variable containing the second half
+    right = set(i[half:])
+
+    #goes through every single letter
+    #assign a number to a letter
+    #and increase the number by 1 every time
+    #because it starts with 0 we need to "+1"
+    for key, char in enumerate(ascii_letters):
+        if char in left and char in right:
+            total += key + 1
+
+print(total)
