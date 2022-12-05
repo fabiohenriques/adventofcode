5cba278fe481f745a135ed638452d87997a84a97
Fabio Henriques <fabyohenriques@gmail.com>
2022-12-05 22:01:31 +0000

    AOC2022 Day 4 Part 2

--- aoc2022/day4/day4.py
+++ aoc2022/day4/day4.py
@@ -4,6 +4,7 @@ with open('day4_input.txt', 'r') as f:
 sections = data.split('\n')
 
 overlap_q1 = 0
+overlap_q2 = 0
 
 for s in sections:
     s1, s2 = s.split(',')
@@ -12,4 +13,8 @@ for s in sections:
     if(s1_start <= s2_start and s2_end <= s1_end) or (s2_start <= s1_start and s1_end <= s2_end):
         overlap_q1 += 1
 
-print('Part 1 answer', overlap_q1)
+    if set(range(s1_start, s1_end + 1)) & set(range(s2_start, s2_end + 1)):
+        overlap_q2 += 1
+
+print('Part 1 answer:', overlap_q1)
+print('Part 2 answer:', overlap_q2)
